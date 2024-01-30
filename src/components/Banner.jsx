a0db203cb2380e6a88a16b13a6db351b0ad4a423
tanvir35web <islammdtanvirul@gmail.com>
2024-01-31 00:29:50 +0600

    slider code refactor and clean

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -3,7 +3,7 @@ import { s1, s2, s3, s4 } from "../assets";
 import { IoArrowBack, IoArrowForwardOutline } from "react-icons/io5";
 
 const Banner = () => {
-  const data = [s1, s2, s3, s4];
+  const data = [ s1, s2, s3, s4 ];
   const [currentSlide, setCurrentSlide] = useState(0);
 
   const prevSlide = () => {
@@ -22,27 +22,16 @@ const Banner = () => {
             style={{ transform: `translate(-${currentSlide * 100}vw)` }}
             className="w-[400vw] h-full flex transition-transform duration-1000"
           >
-            <img
-              className="w-screen h-full object-cover"
-              src={data[0]}
-              alt=""
-            />
-            <img
-              className="w-screen h-full object-cover"
-              src={data[1]}
-              alt=""
-            />
-            <img
-              className="w-screen h-full object-cover"
-              src={data[2]}
-              alt=""
-            />
-            <img
-              className="w-screen h-full object-cover"
-              src={data[3]}
-              alt=""
-            />
+            {data.map((image) => (
+              <img
+                key={image}
+                className="w-screen h-full object-cover"
+                src={image}
+                alt="Hero Image"
+              />
+            ))}
           </div>
+
           <div className="flex absolute w-fit left-0 right-0 mx-auto gap-8 bottom-44">
             <div
               onClick={prevSlide}
