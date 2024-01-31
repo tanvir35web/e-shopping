526fa5563fe3e8508b94c41a21bc06fff0e1fdf2
tanvir35web <islammdtanvirul@gmail.com>
2024-01-31 13:41:09 +0600

    product cart complete with transiton animation

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,22 +1,31 @@
-import React from 'react'
-import ProductsCard from './ProductsCard';
+import React from "react";
+import ProductsCard from "./ProductsCard";
 
-const Products = ( {products} ) => {
+const Products = ({ products }) => {
 
-  console.log(products);
-  
   return (
-    <div className='py-10'>
-        <div className='flex flex-col items-center gap-4'>
-            <h1 className='bg-black text-white text-2xl w-80 p-2 text-center'>Shopping Everyday</h1>
-            <span className='w-20 h-[3px] bg-black rounded-xl'></span>
-            <p className='max-w-[700px] text-center text-gray-600'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos, voluptates assumenda debitis sit voluptas, officia numquam ea recusandae quam impedit facilis voluptatibus nobis fugit? Ad, tenetur? Tenetur quasi quos facilis blanditiis animi odio qui accusamus soluta. Minima provident aspernatur eligendi dolor fugiat hic, quia reprehenderit tenetur, magni, atque officiis iste.</p>
-        </div>
-        <div className='max-w-screen-xl mx-auto'>
-            <ProductsCard/>
-        </div>
+    <div className="py-10">
+      <div className="flex flex-col items-center gap-4">
+        <h1 className="bg-black text-white text-2xl w-80 p-2 text-center">
+          Shopping Everyday
+        </h1>
+        <span className="w-20 h-[3px] bg-black rounded-xl"></span>
+        <p className="max-w-[700px] text-center text-gray-600">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos,
+          voluptates assumenda debitis sit voluptas, officia numquam ea
+          recusandae quam impedit facilis voluptatibus nobis fugit? Ad, tenetur?
+          Tenetur quasi quos facilis blanditiis animi odio qui accusamus soluta.
+          Minima provident aspernatur eligendi dolor fugiat hic, quia
+          reprehenderit tenetur, magni, atque officiis iste.
+        </p>
+      </div>
+      <div className="max-w-screen-xl mx-auto py-10 grid grid-cols-4 gap-10">
+        {products.map((item) => (
+          <ProductsCard key={item._id} product={item} />
+        ))}
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default Products;
--- src/components/ProductsCard.jsx
+++ src/components/ProductsCard.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import React from "react";
+import { BsArrowRight } from "react-icons/bs";
 
-const ProductsCard = () => {
+const ProductsCard = ({ product }) => {
+  console.log(product);
   return (
-    <div>ProductsCard</div>
-  )
-}
+    <div className="group">
+      <div className="w-full h-96 overflow-hidden cursor-pointer">
+        <img
+          className="w-full h-full object-cover group-hover:scale-110 duration-500"
+          src={product.image}
+          alt="product Image"
+        />
+      </div>
 
-export default ProductsCard
+      <div className="w-full border-[1px] px-2 py-4 flex justify-between">
+        <div>
+          <h2 className="text-base font-semibold">
+            {product.title.substring(0, 15)}
+          </h2>
+        </div>
+        <div className="flex justify-end gap-2 relative overflow-hidden w-28 text-sm">
+          <div className="flex flex-row gap-2 transform group-hover:translate-x-24 transition-transform duration-500">
+            <p className="line-through text-gray-500">$ {product.oldPrice}</p>
+            <p className="font-semibold">$ {product.price}</p>
+          </div>
+          <p className="absolute z-20 w-[100] text-gray-500 hover:text-gray-900 flex items-center gap-1 top-0 transform -translate-x-32 group-hover:translate-x-0 transition-transform cursor-pointer duration-500 ">
+            {" "}
+            add to cart{" "}
+            <span>
+              {" "}
+              <BsArrowRight />{" "}
+            </span>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProductsCard;
