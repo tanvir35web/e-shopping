d8641efd1e125c13a55fbea0e81f3b80382b07a2
tanvir35web <islammdtanvirul@gmail.com>
2024-01-31 23:31:39 +0600

    bug fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.css";
-import { Home } from "./pages/home";
+import { Home } from "./pages/Home";
 import { Header } from "./components/Header";
 import Footer from "./components/Footer";
 import Cart from "./pages/Cart";
