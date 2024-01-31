aac4ee20f0ead18b9ef73e075d4f9acfb307b10e
tanvir35web <islammdtanvirul@gmail.com>
2024-01-31 14:25:47 +0600

    Nav item font size set to semi bold

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,11 +10,11 @@ export const Header = () => {
         </div>
         <div className="flex items-center gap-8" > 
             <ul className="flex items-center gap-8">
-                <li className="text-base text-black font-bold hover:text-orange-900 hover:underline underline-offset-2 decoration-[1px] cursor-pointer "> Home </li>
-                <li className="text-base text-black font-bold hover:text-orange-900 hover:underline underline-offset-2 decoration-[1px] cursor-pointer "> Pages </li>
-                <li className="text-base text-black font-bold hover:text-orange-900 hover:underline underline-offset-2 decoration-[1px] cursor-pointer "> shop </li>
-                <li className="text-base text-black font-bold hover:text-orange-900 hover:underline underline-offset-2 decoration-[1px] cursor-pointer "> Element </li>
-                <li className="text-base text-black font-bold hover:text-orange-900 hover:underline underline-offset-2 decoration-[1px] cursor-pointer "> Blog </li>
+                <li className="text-base text-black font-semibold hover:text-orange-900 hover:underline underline-offset-2 decoration-[1px] cursor-pointer "> Home </li>
+                <li className="text-base text-black font-semibold hover:text-orange-900 hover:underline underline-offset-2 decoration-[1px] cursor-pointer "> Pages </li>
+                <li className="text-base text-black font-semibold hover:text-orange-900 hover:underline underline-offset-2 decoration-[1px] cursor-pointer "> shop </li>
+                <li className="text-base text-black font-semibold hover:text-orange-900 hover:underline underline-offset-2 decoration-[1px] cursor-pointer "> Element </li>
+                <li className="text-base text-black font-semibold hover:text-orange-900 hover:underline underline-offset-2 decoration-[1px] cursor-pointer "> Blog </li>
             </ul>
             <div className="w-20 bg-amber-200 p-3 rounded-md flex gap-4">
                 <img className="w-6" src={ cartIcon } alt="Cart image" />
