bf7be9fa4bffed9e6c2899a2fdf4f308cf885784
tanvir35web <islammdtanvirul@gmail.com>
2024-02-05 21:35:07 +0600

    responsive side nav bar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,22 +1,58 @@
-import React from "react";
+import React, { useState } from "react";
 import { cartIcon, darkLogo, userImg } from "../assets";
 import { Link } from "react-router-dom";
 import { useSelector } from "react-redux";
+import { CgMenuRight } from "react-icons/cg";
+import { IoMdClose } from "react-icons/io";
+import "./Header.css";
 
 export const Header = () => {
     const productData = useSelector((state) => state.shopping.productData);
     const userInfo = useSelector((state) => state.shopping.userInfo);
+    const [showNav, setShowNav] = useState(false);
+
+    const toggleNav = () => {
+        setShowNav(!showNav);
+        console.log(showNav);
+    };
 
     return (
-        <div className="w-full h-20 bg-white border-b-[1px] border-b-gray-400 font-bodyFont sticky top-0 z-50">
+        <div className="w-full h-20 bg-white border-b-[1px] border-b-gray-400 font-bodyFont sticky top-0 z-50 p-2">
             <div className="max-w-screen-xl h-full mx-auto flex items-center justify-between">
                 <Link to="/">
                     <div>
                         <img className="w-32" src={darkLogo} alt="dark logo" />
                     </div>
                 </Link>
-                <div className="flex items-center gap-8">
-                    <ul className="flex items-center gap-8">
+
+                <div className="md:hidden">
+                    <button
+                        onClick={toggleNav}
+                        className="text-black p-2 focus:outline-none"
+                    >
+                        <CgMenuRight className="text-2xl" />
+                    </button>
+                </div>
+
+                <div
+                    className={` ${
+                        showNav ? "sideNav" : "hidden"
+                    } items-center gap-8 md:flex`}
+                >
+                    <ul
+                        className={`flex items-center gap-8 ${
+                            showNav ? "flex-col" : "flex-row"
+                        } `}
+                    >
+                        {showNav ? (
+                            <IoMdClose
+                                onClick={toggleNav}
+                                className="mt-8 text-3xl"
+                            />
+                        ) : (
+                            ""
+                        )}
+
                         <Link to="/">
                             <li className="text-base text-black font-semibold hover:text-orange-900 hover:underline underline-offset-2 decoration-[1px] cursor-pointer ">
                                 Home
@@ -63,10 +99,6 @@ export const Header = () => {
                                           }
                                 }
                             />
-
-                            {userInfo && (
-                                <p className="font-medium"> {userInfo.name} </p>
-                            )}
                         </div>
                     </Link>
                 </div>
