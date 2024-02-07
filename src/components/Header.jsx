0401693de0b8b674858c042fbfa5eea7d309c435
tanvir35web <islammdtanvirul@gmail.com>
2024-02-07 16:17:14 +0600

    side nav bar bug fixed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -54,25 +54,25 @@ export const Header = () => {
                         )}
 
                         <Link to="/">
-                            <li className="text-base text-black font-semibold hover:text-orange-900 hover:underline underline-offset-2 decoration-[1px] cursor-pointer ">
+                            <li onClick={toggleNav} className="text-base text-black font-semibold hover:text-orange-900 hover:underline underline-offset-2 decoration-[1px] cursor-pointer ">
                                 Home
                             </li>
                         </Link>
-                        <li className="text-base text-black font-semibold hover:text-orange-900 hover:underline underline-offset-2 decoration-[1px] cursor-pointer ">
+                        <li onClick={toggleNav} className="text-base text-black font-semibold hover:text-orange-900 hover:underline underline-offset-2 decoration-[1px] cursor-pointer ">
                             Pages
                         </li>
-                        <li className="text-base text-black font-semibold hover:text-orange-900 hover:underline underline-offset-2 decoration-[1px] cursor-pointer ">
+                        <li onClick={toggleNav} className="text-base text-black font-semibold hover:text-orange-900 hover:underline underline-offset-2 decoration-[1px] cursor-pointer ">
                             shop
                         </li>
-                        <li className="text-base text-black font-semibold hover:text-orange-900 hover:underline underline-offset-2 decoration-[1px] cursor-pointer ">
+                        <li onClick={toggleNav} className="text-base text-black font-semibold hover:text-orange-900 hover:underline underline-offset-2 decoration-[1px] cursor-pointer ">
                             Element
                         </li>
-                        <li className="text-base text-black font-semibold hover:text-orange-900 hover:underline underline-offset-2 decoration-[1px] cursor-pointer ">
+                        <li onClick={toggleNav} className="text-base text-black font-semibold hover:text-orange-900 hover:underline underline-offset-2 decoration-[1px] cursor-pointer ">
                             Blog
                         </li>
                     </ul>
                     <Link to="/cart">
-                        <div className="w-20 bg-amber-200 p-3 rounded-md flex gap-4">
+                        <div onClick={toggleNav} className="w-20 bg-amber-200 p-3 rounded-md flex gap-4">
                             <img
                                 className="w-6"
                                 src={cartIcon}
@@ -85,7 +85,7 @@ export const Header = () => {
                     </Link>
 
                     <Link to="/login">
-                        <div className="flex gap-3 items-center ">
+                        <div onClick={toggleNav} className="flex gap-3 items-center ">
                             <img
                                 className="w-9 h-9 rounded-full"
                                 src={userInfo ? userInfo.image : userImg}
